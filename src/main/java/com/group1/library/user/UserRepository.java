6d5eee5be00d331102f41f2a1eaf10963da06731
bastienPuigrodon <bastien.puigroon@gmail.com>
2020-03-17 08:52:17 +0100

    Remove Import JDBC
    Done by Bastien

--- src/main/java/com/group1/library/user/UserRepository.java
+++ src/main/java/com/group1/library/user/UserRepository.java
@@ -1,8 +1,6 @@
 package com.group1.library.user;
 
 import com.group1.library.user.User;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
