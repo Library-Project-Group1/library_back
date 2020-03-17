104713beb4101692d10c85bf503b16f396cea739
bastienPuigrodon <bastien.puigroon@gmail.com>
2020-03-17 16:07:44 +0100

    Update User.java

--- src/main/java/com/group1/library/user/User.java
+++ src/main/java/com/group1/library/user/User.java
@@ -1,16 +1,14 @@
 package com.group1.library.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name = "users")
 public class User implements Serializable {
     //ATTRIBUTES
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
