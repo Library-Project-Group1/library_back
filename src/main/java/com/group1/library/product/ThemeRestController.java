be4705c398e96a4781427046e7dbb58943ab8ecb
HenryKienlen <61804378+HenryKienlen@users.noreply.github.com>
2020-03-17 12:11:33 +0100

    ThemeRestController et service

---
+++ src/main/java/com/group1/library/product/ThemeRestController.java
@@ -0,0 +1,46 @@
+package com.group1.library.product;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.awt.*;
+import java.util.Optional;
+
+
+@RestController
+public class ThemeRestController {
+
+
+    @Autowired
+    private ThemeServiceImpl themeService;
+
+    @RequestMapping("/themes")
+    public Iterable<Theme> themes(){
+        return themeService.getAll();
+    }
+
+    @GetMapping("/themes/{id}")
+    public Theme findById(@PathVariable Long id){
+        return themeService.getById(id);
+    }
+
+    @PostMapping(path = "/themes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> createTheme(@RequestBody @Valid Theme theme){
+        try{
+            themeService.add(theme);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (ThemeAlreadyExistsException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+
+    }
+
+
+}
--- src/main/java/com/group1/library/product/ThemeServiceImpl.java
+++ src/main/java/com/group1/library/product/ThemeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.group1.library.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ThemeServiceImpl implements AttributeProductService<Theme,Long> {
 
     //Attributes
