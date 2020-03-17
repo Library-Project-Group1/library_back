3efa1a08205b6bffb096a2801d87e4792b7b05c3
HenryKienlen <61804378+HenryKienlen@users.noreply.github.com>
2020-03-17 10:22:42 +0100

    Create ThemeServiceImpl.java

---
+++ src/main/java/com/group1/library/product/ThemeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.group1.library.product;
+
+import java.util.List;
+
+public class ThemeServiceImpl  {
+
+
+    @Override
+    public Object getById(Object id) {
+        return null;
+    }
+
+    @Override
+    public void edit(Object o) {
+
+    }
+
+    @Override
+    public void remove(Object o) {
+
+    }
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+}
