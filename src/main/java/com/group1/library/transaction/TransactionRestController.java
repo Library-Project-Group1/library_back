ae01eee585f899a0e4fba25d4999c26dff052919
Pauline-Humbert <Pauline-Humbert@hotmail.fr>
2020-03-23 17:16:56 +0100

    update transactionrestcontroller

--- src/main/java/com/group1/library/transaction/TransactionRestController.java
+++ src/main/java/com/group1/library/transaction/TransactionRestController.java
@@ -5,11 +5,15 @@ import com.group1.library.product.ProductNotFoundException;
 import com.group1.library.user.User;
 import com.group1.library.user.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/apiTransaction")
 public class TransactionRestController {
 
     //ATTRIBUTES
@@ -17,7 +21,8 @@ public class TransactionRestController {
     private TransactionServiceImpl transactionService;
 
     //METHODS
-    public void createTransaction(Transaction transaction, User user, Product product){
+    @PostMapping(value = "/createTransaction", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void createTransaction(@RequestBody @Valid Transaction transaction, @RequestBody @Valid User user, @RequestBody @Valid Product product){
         try {
             this.transactionService.addTransaction(transaction, user, product);
         }catch (TransactionNotSuccesfull e){
@@ -25,7 +30,8 @@ public class TransactionRestController {
         }
     }
 
-    public Transaction findTransactionById(Long id) {
+    @GetMapping("/transaction/{id}")
+    public Transaction findTransactionById(@PathVariable("id") Long id) {
         try{
             return this.transactionService.findTransactionById(id);
         }catch (TransactionNotFound e){
@@ -34,7 +40,8 @@ public class TransactionRestController {
         }
     }
 
-    public Iterable<Transaction> getTransactionByUser(User user) {
+    @GetMapping("/transaction/user")
+    public Iterable<Transaction> getTransactionByUser(@RequestBody @Valid User user) {
         try {
             return this.transactionService.getTransactionByUser(user);
         }catch (UserNotFoundException e){
@@ -43,7 +50,8 @@ public class TransactionRestController {
         }
     }
 
-    public Iterable<Transaction> getTransactionByProduct(Product product) {
+    @GetMapping("/transaction/product")
+    public Iterable<Transaction> getTransactionByProduct(@RequestBody @Valid Product product) {
         try {
             return this.transactionService.getTransactionByProduct(product);
         }catch (ProductNotFoundException e){
@@ -52,6 +60,7 @@ public class TransactionRestController {
         }
     }
 
+    @GetMapping("/allTransaction")
     public Iterable<Transaction> getAll() {
         return this.transactionService.getAll();
     }
