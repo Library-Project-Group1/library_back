3b29536e92bca6f0e72772a96d8aef979d794258
bastienPuigrodon <bastien.puigroon@gmail.com>
2020-03-23 13:48:02 +0100

    Update LibraryApplication.java

--- src/main/java/com/group1/library/LibraryApplication.java
+++ src/main/java/com/group1/library/LibraryApplication.java
@@ -28,7 +28,7 @@ public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
-        System.out.println("****** INTIATION SUCCESFULL !!!*********");
+        System.out.println("****** INTIATION SUCCESFULL !!!*********");//
 
 
     }
